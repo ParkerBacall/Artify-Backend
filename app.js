cc192628e30d3969a5e285c54d7a6119473e3aa2
ParkerBacall <pbacall2@gmail.com>
2020-02-26 15:57:00 -0700

    show user authorized route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const user = require('./routes/user')
 const login = require('./routes/login')
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const queries = require('../db/queries')
+const jwt =require('jsonwebtoken')
+const SECRET = "fuckboi"
+
+
+router.get("/users", (request, response) =>{
+    if(!request.headers.authorization){
+        response.status(401).json({message: "fuckass"})
+    }else{
+        const token = request.headers.authorization.split(" ")[1]
+        jwt.verify(token, SECRET, (error, decodedToken)=>{
+            response.send(decodedToken)
+        })
+    }
+})
 
 router.post("/users", (request, response) =>{
     queries.user.create(request.body)
-    .then(results => response.send(results))
-    .then(response.status(201))
-    
+    .then(user => jwt.sign(user, SECRET, (error, token) => {
+        response.status(200).json({ token })}))
 })
 
 module.exports = router

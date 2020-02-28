31860de38b1ca5f2628b9b3c4de7823d69ce3f9a
ParkerBacall <pbacall2@gmail.com>
2020-02-28 16:09:16 -0700

    refactoring

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgresql://localhost/art_db'
+    connection: 'postgresql:///art_db'
   },
 
   production: {
--- routes/gene.js
+++ routes/gene.js
@@ -1,9 +1,24 @@
-const express = require("express")
-const router = express()
-const queries = require('../db/queries')
+express = require("express")
+router = express()
+const fetch = require('node-fetch')
+const count = 50
 
-router.get('/genes', (request, response) => {
-    queries.gene.listAll().then(genes =>  response.send(genes))
-})
+xappToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI1ZTU4NDFmOGViOWE2ODAwMTIwODJhZjQiLCJleHAiOjE1ODM0NDk2NzQsImlhdCI6MTU4Mjg0NDg3NCwiYXVkIjoiNWU1ODQxZjhlYjlhNjgwMDEyMDgyYWY0IiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjVlNTg0YmNhY2E3N2E2MDAxMjdjM2MyMCJ9.Hup__6PY57_H_S9A6mkl4_11Ll60japGzKl9tG4wbfg';
+
+
+
+fetch(`https://api.artsy.net/api/genes?size=${count}`,{
+    method: 'GET',
+    headers: {
+      'X-Xapp-Token': xappToken,
+      'Accept': 'application/vnd.artsy-v2+json'
+    }
+  })
+  .then(response => response.json())
+  .then(art => {
+    router.get('/genes', (request, response) =>{
+        response.send(art._embedded.genes)
+    })
+  })
 
   module.exports = router

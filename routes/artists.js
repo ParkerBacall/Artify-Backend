22da164838df8e850578ba0dc23bc584866eaa6e
ParkerBacall <pbacall2@gmail.com>
2020-03-05 11:30:02 -0700

    add test route

--- routes/artists.js
+++ routes/artists.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const queries = require('../db/queries')
 
+router.get("/artists", (request, response) => {
+    response.send("hey")
+})
+
 router.post("/artists", (request, response) => {
     queries.artists.create(request.body).then(artist => response.send(artist))
 }

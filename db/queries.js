3e71b9ad8b098cb1c35eca027944a7f506642c46
parkerbacall <Pbacall2@gmail.com>
2021-02-24 10:06:33 -0800

    uupdate secret

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 const database = require('./database')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const SECRET = "fuckboi" || process.env['SECRET_KEY_BASE']
+const SECRET = "18ie912" || process.env['SECRET_KEY_BASE']
 
 module.exports = {
     genre: {
